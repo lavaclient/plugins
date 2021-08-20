791a0e9678102c8221f889dc2be69f00efaa095d
melike2d <gino@dimensional.fun>
2021-08-20 12:12:12 -0700

    :ok_hand: rewrite queue plugin

--- packages/queue/src/Queue.ts
+++ packages/queue/src/Queue.ts
@@ -1,198 +1,126 @@
-import { EventEmitter } from "events";
 import { Song } from "./Song";
+import { DiscordResource, getId, NodeEvents, Player, Snowflake } from "lavaclient";
+import { mayStartNext, Track } from "@lavaclient/types";
+import { TypedEmitter } from "tiny-typed-emitter";
 
-import type { Player } from "lavaclient";
+export enum LoopType {
+    None, Queue, Song
+}
 
-export class Queue extends EventEmitter {
-    /**
-     * The tracks that are in this queue.
-     */
-    readonly tracks: Song[];
+export class Queue extends TypedEmitter<QueueEvents> {
+    tracks: Song[] = [];
+    previous: Song[] = [];
+    loop: Loop = { type: LoopType.None, current: 0, max: -1 };
+    last: Song | null = null;
+    current: Song | null = null;
 
-    /**
-     * The player for this queue.
-     */
-    readonly player: Player;
-
-    /**
-     * The previously played songs.
-     */
-    previous: Song[];
-
-    /**
-     * Whether this queue has been started or not.
-     */
-    started: boolean;
-
-    /**
-     * The currently playing song.
-     */
-    current?: Song;
-
-    /**
-     * The current length of the queue.
-     */
-    length: number;
-
-    /**
-     * The type of loop that is occurring.
-     */
-    private _loop?: "song" | "queue";
-
-    /**
-     * @param player
-     */
-    constructor(player: Player) {
+    constructor(readonly player: Player) {
         super();
 
-        player.manager.emit("queueCreate", this);
+        player.on("trackStart", () => {
+            if (!this.current) {
+                /* shouldn't really happen but oh well. */
+                return
+            }
 
-        this.player = player
-            .on("end", (event) => {
-                if (event && ![ "REPLACED" ].includes(event.reason)) {
-                    this.emit("trackEnd", this.current);
-                    if (this._loop === "song") {
-                        this.tracks.unshift(this.current!);
-                    } else {
-                        this.previous.unshift(this.current!);
-                    }
+            if (this.loop.type === LoopType.Song && this.current === this.last) {
+                this.loop.current++;
+            }
 
-                    this._next();
-                    if (!this.current) {
-                        if (this._loop === "queue") {
-                            this.tracks.push(...this.previous.reverse());
-                            this.previous = [];
-                            return this.start();
-                        }
+            this.emit("trackStart", this.current);
+        });
 
-                        this.emit("finished");
-                        return;
-                    }
+        player.on("trackEnd", (_, reason) => {
+            if (!mayStartNext[reason]) {
+                return;
+            }
 
-                    return this.player.play(this.current.track);
-                }
-            })
-            .on("start", () => this.emit("trackStart", this.current));
+            this.last = this.current;
+            switch (this.loop.type) {
+                case LoopType.Song:
+                    return this.player.play(this.current!, {});
+                case LoopType.Queue:
+                    this.previous.push(this.current!);
+                    break
+            }
 
-        this.tracks = [];
-        this.previous = [];
-        this.started = false;
-        this.length = 0;
+            if (!this.tracks.length) {
+                this.tracks = this.previous;
+                this.previous = [];
+            }
+
+            this.emit("trackEnd", this.current!);
+            return this.next();
+        });
     }
 
-    /**
-     * The current type of loop that is occurring.
-     */
-    get loopType(): "song" | "queue" | undefined {
-        return this._loop;
-    }
-
-    /**
-     * Skips the current song and returns the new playing one.
-     * @since 2.0.1
-     */
-    async skip(): Promise<Song | undefined> {
+    async skip(): Promise<Song | null> {
         await this.player.stop();
         return this.current;
     }
 
-    /**
-     * Start the queue.
-     * @since 1.0.0
-     */
     async start(): Promise<boolean> {
-        if (!this.current) {
-            this._next();
-        }
+        return this.next();
+    }
 
-        if (!this.current) {
+    async next(): Promise<boolean> {
+        const next = this.tracks.shift();
+        if (!next) {
+            this.emit("finish");
             return false;
         }
 
-        await this.player.play(this.current.track);
-
-        return this.started = true;
+        this.current = next;
+        await this.player.play(next, {});
+        return true;
     }
 
-    /**
-     * Add songs to the queue.
-     * @param songs The song(s) to add.
-     * @param requester The user that requested this song.
-     * @since 1.00.
-     */
-    add(songs: Addable | Array<Addable>, requester?: string | Record<string, any>): number {
-        const requesterId = requester
-            ? typeof requester === "string"
-                ? requester
-                : requester.id
-            : null;
-        for (const song of (Array.isArray(songs) ? songs : [ songs ])) {
-            let toAdd: Song | null = null;
-            if (song instanceof Song) {
-                toAdd = song;
-            } else if (typeof song === "string") {
-                toAdd = new Song(song, requesterId);
-            } else if (typeof song === "object") {
-                toAdd = new Song(song.track, requesterId);
-            }
-
-            if (toAdd) {
-                this.tracks.push(toAdd);
-                this.length++;
-            }
-        }
-
-        return this.length;
+    emit<U extends keyof QueueEvents>(event: U, ...args: Parameters<QueueEvents[U]>): boolean {
+        const _event: keyof NodeEvents = event === "finish" ? "queueFinish" : event;
+        // @ts-expect-error i dont wanna deal with this lol.
+        this.player.node.emit(_event, this, ...args);
+        return this.emit(event, ...args);
     }
 
-    emit(event: string, ...args: any[]): boolean {
-        if (!event.startsWith("_")) {
-            const _event = event === "finished" ? "queueFinished" : event;
-            if (this.player.manager.listenerCount(_event)) {
-                this.player.manager.emit(_event, this, ...args);
-            }
-        }
+    add(songs: Addable | Array<Addable>, requester?: Snowflake | DiscordResource): number {
+        const requesterId = requester && getId(requester),
+            toAdd = Array.isArray(songs) ? songs : [ songs ];
 
-        return super.emit(event, ...args);
+        this.tracks.push(...toAdd
+            .map(song => song instanceof Song ? song : new Song(song, requesterId)));
+
+        return this.tracks.length;
     }
 
-    /**
-     * Loop the track or queue.
-     * @param type
-     * @since 2.0.1
-     */
-    loop(type: "queue" | "song"): Queue {
-        this._loop = this._loop === type ? undefined : type;
+    setLoop(type: LoopType, max = this.loop.max): Queue {
+        this.loop.type = type;
+        this.loop.max = max;
+
         return this;
     }
 
-    /**
-     * Sort the queued songs.
-     * @param predicate
-     * @since 1.0.0
-     */
     sort(predicate?: (a: Song, b: Song) => number): Array<Song> {
         return this.tracks.sort(predicate);
     }
 
-    /**
-     * Shuffle all the tracks in the queue.
-     * @since 1.0.0
-     */
     shuffle(): void {
         for (let i = this.tracks.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [ this.tracks[i], this.tracks[j] ] = [ this.tracks[j], this.tracks[i] ];
         }
     }
-
-    /**
-     * Get the next song in the queue.
-     * @private
-     */
-    private _next() {
-        return (this.current = this.tracks.shift());
-    }
 }
 
-export type Addable = string | Record<string, any> | Song;
+export type Addable = string | Track | Song;
+
+export interface QueueEvents {
+    trackStart: (song: Song) => void;
+    trackEnd: (song: Song) => void;
+    finish: () => void;
+}
+
+export interface Loop {
+    type: LoopType;
+    current: number;
+    max: number;
+}
