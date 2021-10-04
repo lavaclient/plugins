0eb4af2d4957f33ede59ab3fa9b71fe7666b10ae
melike2d <gino@femboy.email>
2021-10-04 15:08:18 -0700

    :fire: remove compiled test files

--- packages/queue/test/index.test.d.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import "../dist";
-import "../register";
-import { TextChannel } from "discord.js";
-declare module "../dist" {
-    interface Queue {
-        channel: TextChannel;
-    }
-}
